bbbff8a2b63f914a8ea6129462cebabbf51483e3
Josh Ackroyd <joshua.ackroyd@winnowsolutions.com>
2023-09-14 19:37:02 +0100

    added year to film details page

--- films/src/components/showFilm/FilmDetails.js
+++ films/src/components/showFilm/FilmDetails.js
@@ -6,7 +6,7 @@ function FilmDetails({ filmData }) {
       <div className="show--top">
         <img src={filmData.imageUrl} alt="poster" className="show--poster" />
         <div className="show--film-info">
-          <h2 className="show--film-title">{filmData.title}</h2>
+          <h2 className="show--film-title">{filmData.title} - {filmData.releaseYear}</h2>
           <div className="show--film-ratings">
             <div className="show--josh-rating">Josh rating: {filmData.joshScore}</div>
             <div className="show--other-ratings">
